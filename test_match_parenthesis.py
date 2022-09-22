c9fe71b5d0448a2e9612840ebf8620c92cd5e924
Sriram Balakrishnan <sriram.kasilingam@mail.utoronto.ca>
2022-09-21 22:06:37 -0400

    Added tests for q2

---
+++ test_match_parenthesis.py
@@ -0,0 +1,35 @@
+import unittest
+from match_parenthesis import (
+    match_parens_per_sentence,
+)
+
+
+class TestMatchParanthesis(unittest.TestCase):
+    def test_single_sentence_with_no_unmatched_paren(self):
+        para = "(I prefer 20 degrees and above)!"
+        result = match_parens_per_sentence(para)
+        self.assertEqual(result, para)
+
+    def test_single_sentence_with_single_unmatched_paren(self):
+        para = "(I prefer 20 degrees and above!"
+        result = match_parens_per_sentence(para)
+        self.assertEqual(result, "(I prefer 20 degrees and above)!")
+
+    def test_single_sentence_with_multiple_unmatched_paren(self):
+        para = "(I prefer (20 degrees (and) above!"
+        result = match_parens_per_sentence(para)
+        self.assertEqual(result, "(I prefer (20 degrees (and) above))!")
+    
+    def test_multiple_sentences_with_single_unmatched_paren(self):
+        para = "(I prefer 20 degrees and above)! (I like the beach and the sun."
+        result = match_parens_per_sentence(para)
+        self.assertEqual(result, "(I prefer 20 degrees and above)! (I like the beach and the sun).")
+    
+    def test_multiple_sentences_with_multiple_unmatched_paren(self):
+        para = "(I prefer (20 degrees and above! (I like the beach and (the sun)."
+        result = match_parens_per_sentence(para)
+        self.assertEqual(result, "(I prefer (20 degrees and above))! (I like the beach and (the sun)).")
+
+
+if __name__ == "__main__":
+    unittest.main()
